99db683e4ff33d8af19a4b7329a9353c3e216faf
杨浩 <847923197@qq.com>
2024-12-03 16:33:07 +0800

    测试

--- sdkjs-plugins/content/showbookmark/main.js
+++ sdkjs-plugins/content/showbookmark/main.js
@@ -6,28 +6,31 @@
     // 确保在页面加载完成后执行
     $(document).ready(function () {
         $('#showBookmark').click(function () {
+            console.log("window1",window)
             var allBookmarksContent = ""; // 存储所有书签的内容
             var aBookmarks;
             window.Asc.plugin.executeMethod('GetAllBookmarksNames', [], function (sOutput) {
+                console.log("window2",window)
                 aBookmarks = sOutput;
-            });
-            if (aBookmarks && aBookmarks.length > 0) {
-                for (let i = 0; i < aBookmarks.length; i++) {
-                    var bookmarkName = aBookmarks[i];
-                    var oRange;
-                    window.Asc.plugin.executeMethod('GetBookmarkRange', [bookmarkName], function (sOutput) {
-                        oRange = sOutput;
-                    });
-                    // 如果范围有效，则获取书签内容
-                    if (oRange) {
-                        var bookmarkText = oRange.GetText();
-                        allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
-                    } else {
-                        allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
+                if (aBookmarks && aBookmarks.length > 0) {
+                    for (let i = 0; i < aBookmarks.length; i++) {
+                        var bookmarkName = aBookmarks[i];
+                        var oRange;
+                        window.Asc.plugin.executeMethod('GetBookmarkRange', [bookmarkName], function (sOutput) {
+                            oRange = sOutput;
+                        });
+                        // 如果范围有效，则获取书签内容
+                        if (oRange) {
+                            var bookmarkText = oRange.GetText();
+                            allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
+                        } else {
+                            allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
+                        }
                     }
                 }
-            }
-            console.log("书签内容：", allBookmarksContent);
+                console.log("书签内容：", allBookmarksContent);
+            });
+
         })
     });
 
