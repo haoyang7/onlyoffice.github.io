c432db72894ebc564e4980f306f8d755a15598d9
杨浩 <847923197@qq.com>
2024-12-03 15:52:40 +0800

    测试

--- sdkjs-plugins/content/showbookmark/main.js
+++ sdkjs-plugins/content/showbookmark/main.js
@@ -1,80 +1,49 @@
 (function (window, undefined) {
-    window.Asc.plugin.init = function (initData) {
-        var me = this
-        // 确保在页面加载完成后执行
-        $(document).ready(function () {
-            $('#showBookmark').click(function () {
-                var allBookmarksContent = ""; // 存储所有书签的内容
-                var aBookmarks;
-                // 官方提供的回调函数，所有操作文档的 API 都可以在这里面使用
-                window.Asc.plugin.executeMethod('GetAllBookmarksNames', function (sOutput) {
-                    aBookmarks = sOutput;
-                });
-                if (aBookmarks && aBookmarks.length > 0) {
-                    for (let i = 0; i < aBookmarks.length; i++) {
-                        var bookmarkName = aBookmarks[i];
-                        var oRange;
-                        window.Asc.plugin.executeMethod('GetBookmarkRange', bookmarkName, function (sOutput) {
-                            oRange = sOutput;
-                        });
-                        // 如果范围有效，则获取书签内容
-                        if (oRange) {
-                            var bookmarkText = oRange.GetText();
-                            allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
-                        } else {
-                            allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
-                        }
+    window.Asc.plugin.init = function () {
+
+    };
+
+    // 确保在页面加载完成后执行
+    $(document).ready(function () {
+        $('#showBookmark').click(function () {
+            var allBookmarksContent = ""; // 存储所有书签的内容
+            var aBookmarks;
+            // 官方提供的回调函数，所有操作文档的 API 都可以在这里面使用
+            window.Asc.plugin.executeMethod('GetAllBookmarksNames', [], function (sOutput) {
+                aBookmarks = sOutput;
+            });
+            if (aBookmarks && aBookmarks.length > 0) {
+                for (let i = 0; i < aBookmarks.length; i++) {
+                    var bookmarkName = aBookmarks[i];
+                    var oRange;
+                    window.Asc.plugin.executeMethod('GetBookmarkRange', [bookmarkName], function (sOutput) {
+                        oRange = sOutput;
+                    });
+                    // 如果范围有效，则获取书签内容
+                    if (oRange) {
+                        var bookmarkText = oRange.GetText();
+                        allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
+                    } else {
+                        allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
                     }
                 }
-                console.log("书签内容：", allBookmarksContent);
-
-                me.callCommand(function () {
-                    try {
-                        var oDocument = Api.GetDocument();
-                        if (oDocument) {
-                            var aBookmarks = oDocument.GetAllBookmarksNames();
-                            if (aBookmarks && aBookmarks.length > 0) {
-                                for (let i = 0; i < aBookmarks.length; i++) {
-                                    var bookmarkName = aBookmarks[i];
-                                    var oRange = oDocument.GetBookmarkRange(bookmarkName);
-                                    // 如果范围有效，则获取书签内容
-                                    if (oRange) {
-                                        var bookmarkText = oRange.GetText();
-                                        allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
-                                    } else {
-                                        allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
-                                    }
-                                }
-                                if (allBookmarksContent) {
-                                    console.log("书签内容：", allBookmarksContent);
-                                    $(window.document).find('#bookmarkContent').html(allBookmarksContent);
-                                } else {
-                                    console.log("文档中没有书签内容");
-                                    $(window.document).find('#bookmarkContent').html("文档中没有书签内容");
-                                }
-                            }
-                        }
-                    } catch (error) {
-                        console.error(error)
-                    }
-                }, false, true, function () {
-                    console.log('ok')
-                })
-            })
-        });
-
-        // 在插件 iframe 之外释放鼠标按钮时调用的函数
-        window.Asc.plugin.onExternalMouseUp = function () {
-            var event = document.createEvent('MouseEvents')
-            event.initMouseEvent('mouseup', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null)
-            document.dispatchEvent(event)
-        }
-
-        window.Asc.plugin.button = function (id) {
-            // 被中断或关闭窗口
-            if (id === -1) {
-                this.executeCommand('close', '')
             }
+            console.log("书签内容：", allBookmarksContent);
+
+        })
+    });
+
+    // 在插件 iframe 之外释放鼠标按钮时调用的函数
+    window.Asc.plugin.onExternalMouseUp = function () {
+        var event = document.createEvent('MouseEvents')
+        event.initMouseEvent('mouseup', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null)
+        document.dispatchEvent(event)
+    }
+
+    window.Asc.plugin.button = function (id) {
+        // 被中断或关闭窗口
+        if (id === -1) {
+            this.executeCommand('close', '')
         }
     }
 })(window, undefined)
