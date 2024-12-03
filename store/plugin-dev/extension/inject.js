c6b91325a0754acdf7725b75f8f5f4ce29374c36
杨浩 <847923197@qq.com>
2024-12-03 09:55:54 +0800

    测试

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "https://haoyoung7.github.io/onlyoffice.github.io/sdkjs-plugins/content/showbookmark/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
