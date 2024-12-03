64ca25597de0d3ece945d3fe9705157853ff4667
杨浩 <847923197@qq.com>
2024-12-03 15:46:49 +0800

    测试

--- sdkjs-plugins/content/showbookmark/main.js
+++ sdkjs-plugins/content/showbookmark/main.js
@@ -1,14 +1,34 @@
 (function (window, undefined) {
     window.Asc.plugin.init = function (initData) {
         var me = this
-        console.log("window", window)
         // 确保在页面加载完成后执行
         $(document).ready(function () {
             $('#showBookmark').click(function () {
+                var allBookmarksContent = ""; // 存储所有书签的内容
+                var aBookmarks;
                 // 官方提供的回调函数，所有操作文档的 API 都可以在这里面使用
-                me.callCommand(function (window) {
-                    console.log("callCommand window", window)
-                    var allBookmarksContent = ""; // 存储所有书签的内容
+                window.Asc.plugin.executeMethod('GetAllBookmarksNames', function (sOutput) {
+                    aBookmarks = sOutput;
+                });
+                if (aBookmarks && aBookmarks.length > 0) {
+                    for (let i = 0; i < aBookmarks.length; i++) {
+                        var bookmarkName = aBookmarks[i];
+                        var oRange;
+                        window.Asc.plugin.executeMethod('GetBookmarkRange', bookmarkName, function (sOutput) {
+                            oRange = sOutput;
+                        });
+                        // 如果范围有效，则获取书签内容
+                        if (oRange) {
+                            var bookmarkText = oRange.GetText();
+                            allBookmarksContent += "书签名称: " + bookmarkName + "<br>书签内容: " + bookmarkText + "<br><br>"; // 将每个书签的内容加入到字符串中
+                        } else {
+                            allBookmarksContent += "书签名称: " + bookmarkName + " 的范围未找到<br><br>";
+                        }
+                    }
+                }
+                console.log("书签内容：", allBookmarksContent);
+
+                me.callCommand(function () {
                     try {
                         var oDocument = Api.GetDocument();
                         if (oDocument) {
@@ -27,7 +47,6 @@
                                 }
                                 if (allBookmarksContent) {
                                     console.log("书签内容：", allBookmarksContent);
-                                    // window.opener.postMessage({ type: "bookmarkContent", content: allBookmarksContent }, "*");
                                     $(window.document).find('#bookmarkContent').html(allBookmarksContent);
                                 } else {
                                     console.log("文档中没有书签内容");
@@ -43,13 +62,6 @@
                 })
             })
         });
-
-        // window.addEventListener("message", function(event) {
-        //     if (event.data.type === "bookmarkContent") {
-        //         var content = event.data.content;
-        //         console.log(content); // 这里可以处理或显示接收到的书签内容
-        //     }
-        // }, false);
 
         // 在插件 iframe 之外释放鼠标按钮时调用的函数
         window.Asc.plugin.onExternalMouseUp = function () {
